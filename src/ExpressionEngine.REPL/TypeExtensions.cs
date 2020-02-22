239dd8bfa8e29b3d3162f4cf8ed172bedaff808c
Giacomo Stelluti Scala <gsscoder@gmail.com>
2020-02-22 11:03:26 +0100

    Fixed an extension method (REPL)

--- src/ExpressionEngine.REPL/TypeExtensions.cs
+++ src/ExpressionEngine.REPL/TypeExtensions.cs
@@ -4,7 +4,5 @@ using System.Reflection;
 
 static class TypeExtensions
 {
-    public static string AssemblyVersion(this Type type) =>
-        ((AssemblyVersionAttribute)type.Assembly.GetCustomAttributes(
-            typeof(AssemblyVersionAttribute)).Single()).Version;
+    public static string AssemblyVersion(this Type type) => type.Assembly.GetName().Version.ToString();
 }
